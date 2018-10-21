25c424f7b6ba05806ee86a869ae3d7401a479833
Julie Yu <44332507+JulieYuLilyskunk@users.noreply.github.com>
2018-10-20 21:38:57 -0700

    Create Fibonacchi.py

---
+++ Exercises/Fibonacchi.py
@@ -0,0 +1,11 @@
+##Limit of 1000
+
+firstval = 1
+addval = 0
+finalval = 0
+print(str(firstval))
+while finalval <= 1000:
+  finalval = firstval+addval
+  addval = firstval
+  firstval = finalval
+  print(str(finalval))
